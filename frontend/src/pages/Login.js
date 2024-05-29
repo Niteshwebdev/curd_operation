1aebd2e4aecd08ac509c103f58557b4849a1ebaa
Niteshwebdev <worldbest178@gmail.com>
2024-05-29 02:26:40 -0700

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -27,7 +27,7 @@ const Login = () => {
     }
 
     try {
-      const res = await axios.post(`http://localhost:4000/loginapi`, formData);
+      const res = await axios.post(`https://curd-operation-wine.vercel.app/loginapi`, formData);
       if(res.status === 200) {
         alert("Login successfully");      
         navigate("/showproduct");

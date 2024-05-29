f573cb5938638c9aef580e472c0e0e0aa32d4183
Niteshwebdev <worldbest178@gmail.com>
2024-05-29 11:04:24 -0700

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: [
-      "http://localhost:3000",
+      "http://curd-operation-wine.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
